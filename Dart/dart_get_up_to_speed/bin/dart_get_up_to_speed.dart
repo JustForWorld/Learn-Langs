511ff521bc8305570e74ba5c3edb70deefa4f259
Iliay Belous <i.l.i.a.y.b.e.l.o.u.s@yandex.ru>
2023-04-01 23:47:09 +0300

    added a loop for multiple function calls

--- Dart/dart_get_up_to_speed/bin/dart_get_up_to_speed.dart
+++ Dart/dart_get_up_to_speed/bin/dart_get_up_to_speed.dart
@@ -2,8 +2,14 @@ import 'lib.dart_get_up_to_speed.dart' as dart_get_up_to_speed;
 import 'dart:io';
 
 void main(List<String> arguments) {
-  num a = num.parse(stdin.readLineSync().toString());
-  num b = num.parse(stdin.readLineSync().toString());
-  String? symbol = stdin.readLineSync();
-  print(dart_get_up_to_speed.calculation(a, b, symbol));
+  print('To exit, press the number 0. To continue, any other number.');
+  int exit = int.parse(stdin.readLineSync().toString());
+  while (exit != 0) {
+    num a = num.parse(stdin.readLineSync().toString());
+    num b = num.parse(stdin.readLineSync().toString());
+    String? symbol = stdin.readLineSync();
+    print(dart_get_up_to_speed.calculation(a, b, symbol));
+    print('To exit, press the number 0. To continue, any other number.');
+    exit = int.parse(stdin.readLineSync().toString());
+  }
 }
--- Dart/dart_get_up_to_speed/bin/lib.dart_get_up_to_speed.dart
+++ Dart/dart_get_up_to_speed/bin/lib.dart_get_up_to_speed.dart
@@ -1,28 +1,18 @@
 dynamic calculation(num a, num b, String? sign) {
   switch (sign) {
     case '+':
-      {
-        return a + b;
-      }
+      return a + b;
     case '-':
-      {
-        return a - b;
-      }
+      return a - b;
     case '*':
-      {
-        return a * b;
-      }
+      return a * b;
     case '/':
-      {
-        if (b != 0) {
-          return a / b;
-        } else {
-          return 'Division by zero!';
-        }
+      if (b != 0) {
+        return a / b;
+      } else {
+        return 'Division by zero!';
       }
     default:
-      {
-        return 'Unknown operation!';
-      }
+      return 'Unknown operation!';
   }
 }
