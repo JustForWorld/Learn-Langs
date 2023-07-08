25a59fdc7b4a732334ff33bda9b1f72a4de8e949
Iliay Belous <i.l.i.a.y.b.e.l.o.u.s@yandex.ru>
2023-07-08 21:49:55 +0300

    Chapter 3. Classwork prata_cw_3.5 done by reading book of Stephen Prata.

---
+++ C/Stephen_Prata/Chapter_3/prata_cw_3.5.c
@@ -0,0 +1,9 @@
+// prata_cw_3.5.c -- Chapter 3; classwork 5
+#include <stdio.h>
+int main(void) {
+	char ch;
+	printf("Введите какой-нибудь символ.\n");
+	scanf("%c", &ch);
+	printf("Код сивола %c равен %d.\n", ch, ch);
+	return 0;
+}
